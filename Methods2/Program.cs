05caa35ad2e324c1436e6d31c3595e42bf5df6c6
sport <sport@LAPTOP-61I2ACB0>
2023-09-15 22:51:17 +0300

    Add project files.

---
+++ Methods2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int n1 = 5;
+int n2 = 6;
+
+Swap(ref n1, ref n2);
+
+Console.WriteLine("Now the 1st number is: " + n1 + " , and the 2nd number is: " + n2); 
+static void Swap(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
